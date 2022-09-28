6dea4a89b934cc5f77b2cf53f8f93b62f248eb34
adhoper <curetmicael@gmail.com>
2022-09-28 11:09:25 -0400

    Mas Cambios

--- script.js
+++ script.js
@@ -80,9 +80,3 @@ closeModal.addEventListener('click',(e)=>{
     modal3.classList.remove('modal--show');
 })*/
 
-function Confirmacion(){
-    swal("El Mensaje se ha enviado", {
-        buttons: false,
-        timer: 1000,
-      });
-}
